f589164e2ebc5edf588b1c505c42fe7efe4177f2
MetriclyFrench <MetriclyFrench@users.noreply.github.com>
2017-05-10 01:05:19 -0700

    Added completed project

---
+++ index.js
@@ -0,0 +1,29 @@
+// Main starting point of application
+// First thing that will execute
+// -------- ES6 Format -------
+// import express from 'express';
+// import http from 'http';
+// import bodyParser from 'body-parser';
+// import morgan from 'morgan';
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
